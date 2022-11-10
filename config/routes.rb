13ff1c118238c0ff6e29d115ae12f846265e52ad
norkitorki <norki@tuta.io>
2022-11-10 11:33:43 +0100

    Generate devise registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'static_pages#home'
+
   get 'static_pages/home'
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+
 end
