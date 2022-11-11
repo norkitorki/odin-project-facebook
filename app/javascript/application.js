e01ef8e9974460405fcb10ddc399839711f101ed
norkitorki <norki@tuta.io>
2022-11-11 07:35:18 +0100

    Add text field to registration form when selecting custom gender

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Handle custom gender registration form input
+const genderInput = document.getElementById("user_gender")
+
+genderInput.addEventListener('change', () => {
+  if(genderInput.value === "Custom" ) {
+    let customInput = '<input type="text" name="user[gender]" id="user_gender">'
+    genderInput.insertAdjacentHTML("afterend", customInput)
+    genderInput.parentElement.removeChild(genderInput)
+  }
+})
