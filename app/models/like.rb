a1080039edc996e1920bfda9d4d2c4990ca94945
norkitorki <norki@tuta.io>
2022-11-11 13:49:34 +0100

    Validate like and user uniqueness

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,17 @@
 class Like < ApplicationRecord
+  before_validation :validate_user
+
   belongs_to :user
   belongs_to :likeable, polymorphic: true
+
+  private
+
+  def validate_user
+    case  
+    when user && likeable.user == user
+      errors.add(:user_id, "cannot like their own #{likeable.class}")
+    when Like.find_by(user: user, likeable: likeable)
+      errors.add(:user_id, "has already liked this #{likeable.class}")
+    end
+  end
 end
