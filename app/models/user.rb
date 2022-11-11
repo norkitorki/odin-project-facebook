ae9201169d81b8fc6c6d7bcc7a443dc6a1741a6e
norkitorki <norki@tuta.io>
2022-11-11 07:34:45 +0100

    Make gender attribute optional

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email, :first_name, :last_name, :birthday, :gender, presence: true
+  validates :email, :first_name, :last_name, :birthday, presence: true
   validates :email, uniqueness: true
 end
