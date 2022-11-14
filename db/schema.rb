0c5b7fe9c9ba2c1a208ab062051a62fec7fb6c63
norkitorki <norki@tuta.io>
2022-11-14 08:49:19 +0100

    Create friend_requests table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_13_101020) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_063604) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_101020) do
     t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.boolean "pending", default: true
+    t.bigint "user_id", null: false
+    t.bigint "candidate_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["candidate_id"], name: "index_friend_requests_on_candidate_id"
+    t.index ["user_id"], name: "index_friend_requests_on_user_id"
   end
 
   create_table "friendships", force: :cascade do |t|
@@ -78,6 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_13_101020) do
   end
 
   add_foreign_key "comments", "users"
+  add_foreign_key "friend_requests", "users"
   add_foreign_key "friendships", "users"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
