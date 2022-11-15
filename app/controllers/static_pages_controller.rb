7fa9b2699c0cc4193893fcd74128bd2aee4a6aaa
norkitorki <norki@tuta.io>
2022-11-15 11:55:38 +0100

    Temporarily display all users in home view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,5 +2,7 @@ class StaticPagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
+    @users = User.all
+    @friend_request = current_user.friend_requests.new
   end
 end
