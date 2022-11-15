cc4146591abe66a95182dab6481ab71ad67ad6d3
norkitorki <norki@tuta.io>
2022-11-15 11:47:59 +0100

    Generate FriendRequestsController

---
+++ app/controllers/friend_requests_controller.rb
@@ -0,0 +1,30 @@
+class FriendRequestsController < ApplicationController
+  def index
+    @friend_requests = current_user.pending_friend_requests.includes(:user)
+  end
+
+  def create
+    @friend_request = current_user.friend_requests.new(friend_request_params)
+    
+    if @friend_request.save
+      flash.now[:notice] = 'Friend request has been send'
+    else
+      flash.now[:alert] = 'Friend request has not been send'
+    end
+  end
+
+  def destroy
+    @friend_request = current_user.pending_friend_requests.find_by(friend_request_params)
+
+    if params[:commit] == 'Accept'
+      current_user.friendships.create(friend: @friend_request.user)
+    end
+    @friend_request.destroy
+  end
+
+  private
+
+  def friend_request_params
+    params.require(:friend_request).permit(:candidate_id)
+  end
+end
