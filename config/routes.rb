9d12d55ac6f02e2f3bcc97afc99cd3cde3015fb3
norkitorki <norki@tuta.io>
2022-11-15 12:53:11 +0100

    Add a route to /sign_in under the devise scope

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   get 'static_pages/home'
 
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
 
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+  end
 end
