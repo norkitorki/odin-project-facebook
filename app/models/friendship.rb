b9b104d4152cafb6584ec59df38f9e763917c871
norkitorki <norki@tuta.io>
2022-11-15 11:50:22 +0100

    Validate previous friendship existence

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,7 +2,7 @@ class Friendship < ApplicationRecord
   after_create :create_inverse, unless: :inverse_exists?
   after_destroy :destroy_inverse, if: :inverse_exists?
 
-  validate :inverse_is_not_user
+  validate :friendship_existence, :inverse_is_not_user
 
   belongs_to :user
   belongs_to :friend, class_name: 'User'
@@ -23,6 +23,14 @@ class Friendship < ApplicationRecord
 
   def inverse_options
     { user_id: friend_id, friend_id: user_id }
+  end
+
+  def friendship_existence
+    errors.add(:friend, 'already exists') if friendship_exists?
+  end
+
+  def friendship_exists?
+    self.class.exists?(user: user, friend: friend)
   end
 
   def inverse_is_not_user
