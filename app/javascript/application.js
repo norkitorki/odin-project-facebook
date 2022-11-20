e48a2a93b0fb2f4994474405cfa0657feaf7d6b7
norkitorki <norki@tuta.io>
2022-11-20 08:48:20 +0100

    Wrap javascript code with a turbo:load event listener

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,13 +2,17 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 
-// Handle custom gender registration form input
-const genderInput = document.getElementById("user_gender")
+document.addEventListener("turbo:load", () => {
+  // Handle custom gender registration form input
+  const genderInput = document.getElementById("user_gender")
 
-genderInput.addEventListener('change', () => {
-  if(genderInput.value === "Custom" ) {
-    let customInput = '<input type="text" name="user[gender]" id="user_gender" class="input">'
-    genderInput.parentElement.classList.remove("select")
-    genderInput.outerHTML = customInput
+  if (genderInput) {
+    genderInput.addEventListener('change', () => {
+      if(genderInput.value === "Custom" ) {
+        let customInput = '<input type="text" name="user[gender]" id="user_gender" class="input">'
+        genderInput.parentElement.classList.remove("select")
+        genderInput.outerHTML = customInput
+      }
+    })
   }
 })
