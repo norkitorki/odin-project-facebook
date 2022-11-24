32fd1bdce43c310a052bb40b9ba448f9246b9b5a
norkitorki <norki@tuta.io>
2022-11-24 08:33:44 +0100

    Define liked_by? and find_or_initialize_like in LikesHelper module

--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,9 @@
 module LikesHelper
+  def liked_by?(user)
+    likes.exists?(user_id: user.id)
+  end
+
+  def find_or_initialize_like(user)
+    likes.find_or_initialize_by(user_id: user.id)
+  end
 end
