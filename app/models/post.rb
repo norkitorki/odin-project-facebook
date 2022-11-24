1998f9549fccb6256faa5eff63e03cbe672f6b14
norkitorki <norki@tuta.io>
2022-11-24 07:15:55 +0100

    Define find_or_initialize_like method

--- app/models/post.rb
+++ app/models/post.rb
@@ -16,4 +16,8 @@ class Post < ApplicationRecord
   def liked_by?(user)
     likes.exists?(user_id: user.id)
   end
+
+  def find_or_initialize_like(user)
+    likes.find_or_initialize_by(user_id: user.id)
+  end
 end
