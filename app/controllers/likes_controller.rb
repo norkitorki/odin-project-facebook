a7201b8259f90fa64ea8b254e67bfd3ff0c94569
norkitorki <norki@tuta.io>
2022-11-25 13:30:44 +0100

    Refactor LikesController to use likeable url query parameters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,25 +1,31 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_path
 
   def create
-    @like = current_user.likes.new(like_params)
-    likeable_class = @like.likeable.class
+    @like = current_user.likes.new(likeable_params)
+    @likeable = @like.likeable
 
     if @like.save
-      flash.now[:notice] = "#{likeable_class} has been liked."
+      redirect_to @path, notice: "#{@likeable.class} has been liked."
     else
-      flash.now[:alert] = "#{likeable_class} has not been liked."
+      redirect_to @path, alert: "#{@likeable.class} has not been liked."
     end
   end
 
   def destroy
     @like = Like.find(params[:id])
     @like.destroy
+
+    redirect_to @path, alert: "#{@like.likeable.class} has been unliked."
   end
 
   private
 
-  def like_params
-    params.require(:like).permit(:likeable_type, :likeable_id)
+  def likeable_params
+    params.require(:likeable).permit(:likeable_type, :likeable_id)
+  end
+
+  def set_path
+    @path = params[:path] || root_path
   end
 end
