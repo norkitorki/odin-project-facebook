48b910c732ef9ed795aa3de20717f2bc38a14fa0
norkitorki <norki@tuta.io>
2022-11-27 08:46:08 +0100

    Create notifications table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_063604) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_27_074011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_063604) do
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.string "body"
+    t.string "target_path"
+    t.boolean "read", default: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id", null: false
@@ -91,5 +101,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_063604) do
   add_foreign_key "friend_requests", "users"
   add_foreign_key "friendships", "users"
   add_foreign_key "likes", "users"
+  add_foreign_key "notifications", "users"
   add_foreign_key "posts", "users"
 end
