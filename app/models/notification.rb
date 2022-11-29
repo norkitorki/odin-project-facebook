7035c3611d2e527168dd3e8d9380884b178c24ad
norkitorki <norki@tuta.io>
2022-11-29 09:01:20 +0100

    Create unread scope in notification model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ApplicationRecord
+  scope :unread, -> { where(read: false).order(created_at: :desc) }
+
   belongs_to :user
 
   validates :body, presence: true, length: { maximum: 100 }
