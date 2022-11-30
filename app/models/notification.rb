a87e31894072e8e2927fea285a2c5a31bba93504
norkitorki <norki@tuta.io>
2022-11-30 10:42:58 +0100

    Define notification add class method

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,11 @@
 class Notification < ApplicationRecord
   scope :unread, -> { where(read: false).order(created_at: :desc) }
 
+  def self.add(body:, user:, target_path: nil)
+    @notification = Notification.create(body: body, user: user, target_path: target_path)
+    ActionCable.server.broadcast('notification', @notification)
+  end
+
   belongs_to :user
 
   validates :body, presence: true, length: { maximum: 100 }
