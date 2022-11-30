da62b223036d09a6b83681706dbdba82379d233b
norkitorki <norki@tuta.io>
2022-11-30 11:26:28 +0100

    Define private set_notifications action in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_notifications
+
+  private
+
+  def set_notifications
+    @notifications = current_user.notifications.order(created_at: :desc) if user_signed_in?
+  end
 end
