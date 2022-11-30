046567085196cc4424dae8d25b8a25f6b40f8515
norkitorki <norki@tuta.io>
2022-11-30 11:06:34 +0100

    Remove unread scope and add class method and configure polymorphic association

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,12 +1,6 @@
 class Notification < ApplicationRecord
-  scope :unread, -> { where(read: false).order(created_at: :desc) }
-
-  def self.add(body:, user:, target_path: nil)
-    @notification = Notification.create(body: body, user: user, target_path: target_path)
-    ActionCable.server.broadcast('notification', @notification)
-  end
-
   belongs_to :user
+  belongs_to :notifiable, polymorphic: true
 
   validates :body, presence: true, length: { maximum: 100 }
 end
