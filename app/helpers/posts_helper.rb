07a8e26496c821e29dc342c37562a33dfcee5bb2
norkitorki <norki@tuta.io>
2022-12-05 08:13:50 +0100

    Define private timeline_posts method in PostsHelper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+  private
+
+  def timeline_posts
+    user_ids = current_user.friends.ids << current_user.id
+    Post.where(user_id: user_ids).includes(:comments, :likes, :user)
+  end
 end
