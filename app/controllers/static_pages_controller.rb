2bafc3750be404388f2f4c605e66ba395d6e3057
norkitorki <norki@tuta.io>
2022-12-13 07:23:56 +0100

    Initialize new post in home action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,7 @@ class StaticPagesController < ApplicationController
 
   def home
     @friend_requests = current_user.pending_friend_requests
+    @post = Post.new
     @posts = timeline_posts
     @timeline = (@friend_requests + @posts).sort { |a, b| b.updated_at <=> a.updated_at }
   end
