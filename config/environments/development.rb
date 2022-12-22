7f949fde5ef631b63f56ae74f5564e33a4ec8e3a
norkitorki <norki@tuta.io>
2022-12-22 12:16:27 +0100

    Remove active storage configuration and routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -69,7 +69,4 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  # Store files locally.
-  config.active_storage.service = :local
 end
