8d69581d1364c995c081926c63981ecda22289a0
norkitorki <norki@tuta.io>
2022-12-23 11:54:59 +0100

    Configure UserMailer to send a welcome email on sign up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    UserMailer.with(user: resource).welcome_email.deliver_later if resource.persisted?
+  end
 
   # GET /resource/edit
   # def edit
