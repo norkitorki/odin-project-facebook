e457f51fb557f59d45192ed9b1eb99c730f8fbd2
norkitorki <norki@tuta.io>
2022-12-23 12:33:58 +0100

    Override private respond_to_on_destroy method

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,4 +24,10 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+
+  def respond_to_on_destroy
+    redirect_to sign_in_path, alert: 'You are now signed out.'
+  end
 end
