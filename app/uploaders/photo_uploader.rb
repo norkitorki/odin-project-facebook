94e2fc649ee657cc732ffe5f8b74ba5545914711
norkitorki <norki@tuta.io>
2022-12-25 07:25:26 +0100

    Use case statement to assign default_url based on model class

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -15,7 +15,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url(*args)
-    model.respond_to?(:gravatar_id) ? model.gravatar_url : ''
+    case model.class.to_s
+    when 'Post' then model.remote_photo || ''
+    when 'User' then model.gravatar_url
+    end
   end
 
   # Process files as they are uploaded:
