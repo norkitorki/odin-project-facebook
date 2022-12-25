f4799f125b80f2c8a1bff938206cf979b30982ad
norkitorki <norki@tuta.io>
2022-12-25 10:36:34 +0100

    Simplify carrierwave photo storage directory path

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -11,7 +11,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     model_id = model.respond_to?(:slug) ? model.slug : model.id
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model_id}"
+    "uploads/#{model.class.to_s.downcase}/#{model_id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
