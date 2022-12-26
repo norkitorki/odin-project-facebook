ff6c2476cc17c7ab979e326cb68a967e4d6dbcea
norkitorki <norki@tuta.io>
2022-12-26 11:46:30 +0100

    Prepend http protocol to body through before_save callback

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,13 @@
 class Link < ApplicationRecord
+  before_save :prepend_protocol
+
   belongs_to :linkable, polymorphic: true
 
   validates :body, presence: true
 
+  private
+
+  def prepend_protocol
+    body.insert(0, 'http://') unless body.start_with?(/https?:\/\//)
+  end
 end
