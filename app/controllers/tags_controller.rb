b7aed2341fd1f4d1ac9222b4c4ded75ee8ed8d52
norkitorki <norki@tuta.io>
2022-12-28 08:14:17 +0100

    Use find_by_tag method to find objects

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,6 @@ class TagsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @objects = TagList.tagables(params[:s])
+    @objects = TagList.find_by_tag(params[:s])
   end
 end
