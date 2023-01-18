7e1e4a8724b6de6311edf88db14e4930920c2368
norkitorki <norki@tuta.io>
2023-01-18 08:07:17 +0100

    Define private tags_by_first_letter method in TagsController

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,13 @@ class TagsController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @objects = TagList.find_by_tag(params[:s])
+  private
+
+  def tags_by_first_letter(tags)
+    tags.each_with_object({}) do |tag, hash|
+      first_letter = tag[0].upcase
+      hash[first_letter] ||= []
+      hash[first_letter] << tag.downcase
+    end.sort
   end
 end
