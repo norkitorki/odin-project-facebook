28981fffb287494f3fceb79a10a6571cad252288
norkitorki <norki@tuta.io>
2023-01-18 08:09:35 +0100

    Define @tag in show action

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,6 +2,10 @@ class TagsController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @tag   = params[:s]
+    @posts = TagList.find_by_tag(@tag)
+  end
+
   private
 
   def tags_by_first_letter(tags)
