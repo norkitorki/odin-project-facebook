e357e9b8e18daa1a136a8534d5357151b3372acf
norkitorki <norki@tuta.io>
2023-01-20 11:59:52 +0100

    Initialize new friend request with a candidate

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -13,8 +13,8 @@ class FriendRequestsController < ApplicationController
     if @friend_request = FriendRequest.find_by(candidate: current_user)
       redirect_to @friend_request, alert: 'User already wants to be your friend.', status: :see_other
     else
-      @friend_request = current_user.friend_requests.new
-      @candidate = User.find(params[:candidate])
+      candidate = User.friendly.find(params[:candidate_id])
+      @friend_request = current_user.friend_requests.new(candidate: candidate)
     end
   end
 
