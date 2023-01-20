29617397bbb0508b3cc58be4e7d42c6a0c3870e4
norkitorki <norki@tuta.io>
2023-01-20 12:36:17 +0100

    Query nav user notifications through the unread scope

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :set_notifications
+  before_action :set_notifications, if: :user_signed_in?
 
   private
 
   def set_notifications
-    @notifications = current_user.notifications.order(created_at: :desc) if user_signed_in?
+    @notifications = current_user.notifications.unread.order(created_at: :desc)
   end
 end
