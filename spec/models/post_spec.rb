d1f959b2b2f1b7162aa913832a8d2ac0bd753c12
norkitorki <norki@tuta.io>
2023-02-03 13:29:54 +0100

    Add user mode specs specific to content presence

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,19 +8,63 @@ RSpec.describe Post, type: :model do
   end
 
   it "should be valid with content and user" do
+    @post.images.destroy_all
+    @post.video = nil
     expect(@post).to be_valid
   end
 
-  it "should be valid without content but with images" do
-    @post.content = nil
-    @post.images << images(:one)
+  it "should be valid with content,user and images" do
+    @post.video = nil
     expect(@post).to be_valid
   end
 
-  it "should be valid without content but with video" do
-    @post.content = nil
-    expect(@post.video).to eq(videos(:one))
+  it "should be valid with content,user and video" do
+    @post.images.destroy_all
     expect(@post).to be_valid
+  end
+
+  context "when content is nil" do
+    before do
+      @post.content = nil
+    end
+
+    it "should be valid with user and images" do
+      @post.video = nil
+      expect(@post).to be_valid
+    end
+
+    it "should be valid with user and video" do
+      @post.images.destroy_all
+      expect(@post).to be_valid
+    end
+
+    it "should be invalid without images and video" do
+      @post.images.destroy_all
+      @post.video = nil
+      expect(@post).to_not be_valid
+    end
+  end
+
+  context "when content is empty" do
+    before do
+      @post.content = ''
+    end
+
+    it "should be valid with user and images" do
+      @post.video = nil
+      expect(@post).to be_valid
+    end
+
+    it "should be valid with user and video" do
+      @post.images.destroy_all
+      expect(@post).to be_valid
+    end
+
+    it "should be invalid without images and video" do
+      @post.images.destroy_all
+      @post.video = nil
+      expect(@post).to_not be_valid
+    end
   end
 
   it "should respond to edited?" do
@@ -31,11 +75,11 @@ RSpec.describe Post, type: :model do
 
   describe '#all_images_destroyed?' do
     it "should return true if images are empty" do
+      @post.images.destroy_all
       expect(@post.all_images_destroyed?).to be true
     end
 
     it "should return true when all images are deleted" do
-      @post.images << images(:one)
       @post.images.first.destroy
       expect(@post.all_images_destroyed?).to be true
     end
