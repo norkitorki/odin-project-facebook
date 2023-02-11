f2aa797cc1abb79c7bf97c3f5534bfb33a42ec59
norkitorki <norki@tuta.io>
2023-02-11 11:48:56 +0100

    Create initial static_page system tests

---
+++ spec/system/static_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :system do
+  include Devise::Test::IntegrationHelpers
+
+  fixtures :users
+
+  before do
+    driven_by(:rack_test)
+    sign_in @user = users(:one)
+  end
+
+  it "should be viewing home" do
+    visit root_path
+    expect(page).to have_current_path(root_path)
+  end
+
+  it "should be viewing discover" do
+    visit root_path
+
+    click_on 'Discover'
+    expect(page).to have_current_path(discover_path)
+  end
+end
