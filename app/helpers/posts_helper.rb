4503bf0152bdf6a1801543e2d863aaf3ecbf2ae7
norkitorki <norki@tuta.io>
2023-03-01 13:48:43 +0100

    Clean up timeline_posts method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,6 +3,10 @@ module PostsHelper
 
   def timeline_posts
     user_ids = current_user.friends.ids << current_user.id
-    Post.where(user_id: user_ids).includes(:comments, :images, :likes, :links, :tag_list, :user, :video)
+    Post.where(user_id: user_ids).includes(preloaded_resources)
+  end
+
+  def preloaded_resources
+    [:comments, :images, :likes, :links, :tag_list, :user, :video]
   end
 end
