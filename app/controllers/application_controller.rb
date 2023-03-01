94ce56b1a96085b559efeef8c1c07c2469b30216
norkitorki <norki@tuta.io>
2023-03-01 13:44:50 +0100

    Define private resource_pagination method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,8 @@ class ApplicationController < ActionController::Base
   def set_notifications
     @notifications = current_user.notifications.unread.order(created_at: :desc)
   end
+
+  def resource_pagination(resources, page, max_count)
+    resources.order(updated_at: :desc).offset((page - 1) * max_count).limit(max_count)
+  end
 end
