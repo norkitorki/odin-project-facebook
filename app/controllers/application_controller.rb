ee941a96e13d68d159607a1dc5f35fd336405406
norkitorki <norki@tuta.io>
2023-03-03 08:58:51 +0100

    Define private set_page method in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
     @notifications = current_user.notifications.unread.order(created_at: :desc)
   end
 
+  def set_page
+    @page = (params[:p] || 1).to_i
+  end
+
   def resource_pagination(resources, page, max_count)
     resources.order(updated_at: :desc).offset((page - 1) * max_count).limit(max_count)
   end
